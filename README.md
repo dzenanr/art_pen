c23f4fd6d2d9a1a6f0103b4d2724ea7a0eea2437
Dzenan Ridjanovic <dzenanr@gmail.com>
2012-10-25 11:47:08 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,7 @@
+#DArt Pen
+
+Drawing a smart dart art. 
+If you are less artistic, use commands, based on the Logo programming language for children, to learn basic programming concepts by drawing.
+
+Dependent on [dartling] (https://github.com/dzenanr/dartling). 
+After cloning it, open the project in Dart Editor with Pub.
---
+++ web/d_art_pen.dart
@@ -0,0 +1,25 @@
+
+import 'dart:html';
+import 'dart:isolate';
+import 'dart:math';
+
+import 'package:dartling/dartling.dart';
+import 'package:dartling/dartling_app.dart';
+
+import 'package:art_pen/art_pen.dart';
+import 'package:art_pen/art_pen_app.dart';
+
+showMinData(ArtRepo minRepo) {
+   var mainView = new View(document, "main");
+   mainView.repo = minRepo;
+   new RepoMainSection(mainView);
+}
+
+main() {
+  var minRepo = new ArtRepo();
+  showMinData(minRepo);
+  var board = new Board(minRepo);
+  new Commands(board.pen);
+}
+
+
